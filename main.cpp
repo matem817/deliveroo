120924b35ec3dedd1ed0aa0dd20563fdd94ae0a0
matem817 <matem817@icloud.com>
2026-02-11 17:25:03 +0100

    Aggiunto menu principale e completata la struttura del programma

--- main.cpp
+++ main.cpp
@@ -1,4 +1,9 @@
 /*
+RICORDATE:
+!!!!!!!!!NON PUBBLICARE GLI ESEGUIBILI :D!!!!!!!!!!!
+Se create un nuovo menù ricordatevi di aggiungere il suo id in posizioni valide 
+e un case dentro lo swirch E, di incrementare lungPosizioniValide
+~Cris
 TODO:
 
 COMMENTI:
@@ -14,27 +19,60 @@ POSIZIONI:
 using namespace std;
 
 int main(){
-    bool esci=false, chiediSel=true, messagiDEBUG=true;
-    int posizioneUtente=0, posizioniValide[]={0}, lungPosizioniValide=1;
+    //variabili di sistema (gestiscono il programma)
+    bool esci=false, chiediSel=false, messagiDEBUG=true;
+    int posizioniValide[]={-1,0},  lungPosizioniValide=2;//<----------- inserite qui i nuovi id -Cris
+    int posizioneUtente=0;
+
+    //variabili di dati utente(immagazzinano info sull'utente)
+
+    //APP
     while(!esci){
-        switch posizioneUtente{
-            case -1:
-                esci=true;
-                if(messagiDEBUG) cout<<"uscita del programma\n";
-                break;
-            case 0://menu principale
-                break;
-        }
         while(chiediSel){
+            chiediSel=true;
             cout<<"=>";
-            cin>>pos;
-            bool valido=false;
+            cin>>posizioneUtente;
             for(int i=0; i<lungPosizioniValide; i++){
-                if(pos==posizioniValide[i]) {
-                    valido=true;
+                if(posizioneUtente==posizioniValide[i]) {
+                    chiediSel=false;
                     break;
                 }
             }
+            if(chiediSel) cout<<"Selezione non valida riprovare\n";
         }
+        chiediSel=true;
+
+        switch (posizioneUtente){
+            case -1:
+                esci=true;
+                cout<<"Grazie per aver usato deliveroo!!!\n";
+                if(messagiDEBUG) cout<<"DEBUG:uscita del programma\n";
+                break;
+
+                
+            case 0://menu principale
+                cout<<"#####################################################"<<endl;
+                cout<<"||@@@@  @@@@@ @     @ @   @ @@@@@ @@@@   @@@   @@@ ||"<<endl;
+                cout<<"||@   @ @     @     @ @   @ @     @   @ @   @ @   @||"<<endl;
+                cout<<"||@   @ @@@@@ @     @ @   @ @@@@@ @@@@  @   @ @   @||"<<endl;
+                cout<<"||@   @ @     @     @  @ @  @     @ @   @   @ @   @||"<<endl;
+                cout<<"||@@@@  @@@@@ @@@@@ @   @   @@@@@ @  @   @@@   @@@ ||"<<endl;
+                cout<<"#####################################################"<<endl;
+                cout<<"Benvenuti su deliveroo"<<endl;
+                cout<<"Digita 0 per vedere questa schermata"<<endl;
+                cout<<"Digita -1 per uscire dal programma"<<endl;
+                break;
+
+    
+            default:
+                cout<<"!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"<<endl;
+                cout<<"OPZIONE NON VALIDA, NON DOVRESTI RIUSCIRCI AD ARRIVARE QUI"<<endl;
+                cout<<"C'E QUALCOSA CHE NON VA NELLO SWITCH O NEL VETTORE"<<endl;
+                cout<<"!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"<<endl;
+                esci=true;
+
+        }
+        if(messagiDEBUG) cout<<"DEBUG:Eseguito un ciclo\n";
+        
     }
 }
