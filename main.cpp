6845204b687e3200186245d5b4b2c118e4f1c29c
matem817 <matem817@icloud.com>
2026-02-11 15:43:14 +0100

    caricata prima parte della struttura dell'app

---
+++ main.cpp
@@ -0,0 +1,39 @@
+/*
+TODO:
+
+COMMENTI:
+
+POSIZIONI:
+-1=esci da programa
+0=menu principale
+*/
+#include<iostream>
+#include<cstdlib>
+#include<ctime>
+using namespace std;
+
+int main(){
+    bool esci=false, chiediSel=true, messagiDEBUG=true;
+    int posizioneUtente=0, posizioniValide[]={0}, lungPosizioniValide=1;
+    while(!esci){
+        switch posizioneUtente{
+            case -1:
+                esci=true;
+                if(messagiDEBUG) cout<<"uscita del programma\n";
+                break;
+            case 0://menu principale
+                break;
+        }
+        while(chiediSel){
+            cout<<"=>";
+            cin>>pos;
+            bool valido=false;
+            for(int i=0; i<lungPosizioniValide; i++){
+                if(pos==posizioniValide[i]) {
+                    valido=true;
+                    break;
+                }
+            }
+        }
+    }
+}
